045014bfc03fff2ccc7b1a756f5bd16d1c8e863b
Marina Adams <bardakover@gmail.com>
2025-05-29 08:09:21 -0700

    Wave1: completed, tested

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,31 @@
 export const drawLetters = () => {
-  // Implement this method for wave 1
-};
+  const letterPool = {
+    A: 9, B: 2, C: 2, D: 4, E: 12,
+    F: 2, G: 3, H: 2, I: 9, J: 1,
+    K: 1, L: 4, M: 2, N: 6, O: 8,
+    P: 2, Q: 1, R: 6, S: 4, T: 6,
+    U: 4, V: 2, W: 2, X: 1, Y: 2,
+    Z: 1,
+  };
+  let letterArray = [];
+  for (let[letter, count] of Object.entries(letterPool)) {
+    for (let i = 0; i < count; i++) {
+      letterArray.push(letter);
+    }
+  }
+
+  let hand = [];
+  while (hand.length < 10) {
+    const randomIndex = Math.floor(Math.random() * letterArray.length);
+    const letter = letterArray[randomIndex];
+    hand.push(letter);
+    letterArray.splice(randomIndex, 1);
+  }
+
+  return hand;
+}
+
+console.log(drawLetters());
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
@@ -13,3 +38,5 @@ export const scoreWord = (word) => {
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
 };
+
+
