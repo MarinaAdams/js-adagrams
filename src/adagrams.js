c48a59af69a49bbbc6496d4bf05577d1736f19d3
Marina Adams <bardakover@gmail.com>
2025-05-29 23:59:47 -0700

    Comleted Wave3, completed test (lines 122-124)

--- src/adagrams.js
+++ src/adagrams.js
@@ -7,6 +7,15 @@ const LETTER_POOL = {
     Z: 1,
   };
 
+const SCORE_CHART = {
+    A: 1, B: 3, C: 3, D: 2, E: 1,
+    F: 4, G: 2, H: 4, I: 1, J: 8,
+    K: 5, L: 1, M: 3, N: 1, O: 1,
+    P: 3, Q: 10, R: 1, S: 1, T: 1,
+    U: 1, V: 4, W: 4, X: 8, Y: 4,
+    Z: 10,
+  };
+
 // ----- Wave 1 -----
 export const drawLetters = () => {
   const letterPool = [];
@@ -27,7 +36,7 @@ export const drawLetters = () => {
   }
 
   return hand;
-}
+};
 
 // ----- Wave 2 -----
 export const usesAvailableLetters = (input, lettersInHand) => {
@@ -45,17 +54,25 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   }
 
   return true;
-}
+};
 
 // ----- Wave 3 -----
-// export const scoreWord = (word) => {
-//   // Implement this method for wave 3
-  
-// };
+export const scoreWord = (word) => {
+  let score = 0;
+
+  for (const letter of word.toUpperCase()) {
+    score += SCORE_CHART[letter] || 0;
+  }
+  if (word.length >= 7 && word.length <= 10) {
+    score += 8;
+  }
+
+  return score;
+};
 
 // ----- Wave 4 -----
 // export const highestScoreFrom = (words) => {
-//   // Implement this method for wave 4
+   // Implement this method for wave 4
 // };
 
 
